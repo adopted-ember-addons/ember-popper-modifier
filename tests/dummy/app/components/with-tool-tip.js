6dd590251d53bd33c1332beee785888cb22a9ac6
Alex LaFroscia <alex@lafroscia.com>
2020-06-22 22:24:37 -0400

    docs: add example of using addon to make a tooltip

---
+++ tests/dummy/app/components/with-tool-tip.js
@@ -0,0 +1,20 @@
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
+
+export default class WithToolTip extends Component {
+  @tracked tooltipVisible = false;
+  @tracked referenceElement;
+
+  @action setReferenceElement(element) {
+    this.referenceElement = element;
+  }
+
+  @action showTooltip() {
+    this.tooltipVisible = true;
+  }
+
+  @action hideTooltip() {
+    this.tooltipVisible = false;
+  }
+}
---
+++ tests/integration/components/cookbook/tool-tip-test.js
@@ -0,0 +1,77 @@
+import { module, test } from "qunit";
+import { setupRenderingTest } from "ember-qunit";
+import { render, triggerEvent } from "@ember/test-helpers";
+import { hbs } from "ember-cli-htmlbars";
+
+module("Integration | Component | cookbook/tool-tip", function (hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(function () {
+    this.showTooltip = false;
+    this.handleMouseOver = () => {
+      this.set("showTooltip", true);
+    };
+    this.handleMouseLeave = () => {
+      this.set("showTooltip", false);
+    };
+    this.setButtonElement = (element) => {
+      this.set("buttonElement", element);
+    };
+  });
+
+  test("rendering a tooltip", async function (assert) {
+    await render(hbs`
+      <button
+        type="button"
+        data-test-button
+        {{did-insert this.setButtonElement}}
+        {{on 'mouseover' this.handleMouseOver}}
+        {{on 'mouseleave' this.handleMouseLeave}}
+      >
+        Hover me!
+      </button>
+
+      {{#if this.showTooltip}}
+        <ToolTip @attachTo={{this.buttonElement}} data-test-tooltip>
+          More Information
+        </ToolTip>
+      {{/if}}
+    `);
+
+    assert.dom("[data-test-tooltip]").doesNotExist();
+
+    await triggerEvent("[data-test-button]", "mouseover");
+
+    assert.dom("[data-test-tooltip]").exists();
+
+    await triggerEvent("[data-test-button]", "mouseleave");
+
+    assert.dom("[data-test-tooltip]").doesNotExist();
+  });
+
+  test("wrapping an element with a tooltip component", async function (assert) {
+    await render(hbs`
+      <WithToolTip @content="More Information" data-test-tooltip as |register show hide|>
+        <button
+          type="button"
+          data-test-button
+          {{did-insert register}}
+          {{on 'mouseover' show}}
+          {{on 'mouseleave' hide}}
+        >
+          Hover me!
+        </button>
+      </WithToolTip>
+    `);
+
+    assert.dom("[data-test-tooltip]").doesNotExist();
+
+    await triggerEvent("[data-test-button]", "mouseover");
+
+    assert.dom("[data-test-tooltip]").exists();
+
+    await triggerEvent("[data-test-button]", "mouseleave");
+
+    assert.dom("[data-test-tooltip]").doesNotExist();
+  });
+});
