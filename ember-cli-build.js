ac86c33dd63b58c8812364626a622ba601ef95d4
Jeldrik Hanschke <admin@jhanschke.de>
2023-01-17 00:46:26 +0100

    restore webpack configuration

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,13 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function (defaults) {
   const app = new EmberAddon(defaults, {
-    // Add options here
+    autoImport: {
+      webpack: {
+        node: {
+          global: true,
+        },
+      },
+    },
   });
 
   /*
