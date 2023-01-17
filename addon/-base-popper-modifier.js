1725418573106f3ed0b6c424845c3559277cab2a
Jeldrik Hanschke <admin@jhanschke.de>
2023-01-17 17:19:23 +0100

    refactor to pure function and plain variable for readability

--- addon/-base-popper-modifier.js
+++ addon/-base-popper-modifier.js
@@ -11,6 +11,46 @@ import {
   endRunLoopModifier as endRunLoop,
 } from './in-runloop-modifier';
 
+function getPopperOptions(positional, named) {
+  // Get an array of just positional "options"; first item is an element reference
+  const positionalArguments = positional.slice(1).filter((arg) => Boolean(arg));
+
+  // Positional args that are not modifiers should be treated as full "options" objects
+  const allPositionalOptions = positionalArguments.filter(
+    (arg) => !isModifier(arg)
+  );
+
+  // Positional args that are modifiers will extend the rest of the configuration
+  const allPositionalModifiers = positionalArguments.filter((arg) =>
+    isModifier(arg)
+  );
+
+  const { ...namedOptions } = named;
+  const options = {
+    ...allPositionalOptions.reduce((acc, curr) => {
+      return { ...acc, ...curr };
+    }, {}),
+    ...namedOptions,
+  };
+
+  // Ensure that the `modifiers` is always an array
+  const modifiers = isEmpty(options.modifiers)
+    ? []
+    : isArray(options.modifiers)
+    ? options.modifiers
+    : [options.modifiers];
+
+  // Add runloop integration and positional modifiers to the array of modifiers
+  options.modifiers = [
+    ...modifiers,
+    ...allPositionalModifiers,
+    beginRunLoop,
+    endRunLoop,
+  ];
+
+  return options;
+}
+
 export default class PopperModifier extends Modifier {
   /** @type {HTMLElement} */
   // eslint-disable-next-line getter-return
@@ -24,65 +64,23 @@ export default class PopperModifier extends Modifier {
     assert('Must implement `referenceElement` property', false);
   }
 
-  initPopperOptions(positional, named) {
-    // Get an array of just positional "options"; first item is an element reference
-    const positionalArguments = positional
-      .slice(1)
-      .filter((arg) => Boolean(arg));
-
-    // Positional args that are not modifiers should be treated as full "options" objects
-    const allPositionalOptions = positionalArguments.filter(
-      (arg) => !isModifier(arg)
-    );
-
-    // Positional args that are modifiers will extend the rest of the configuration
-    const allPositionalModifiers = positionalArguments.filter((arg) =>
-      isModifier(arg)
-    );
-
-    const { ...namedOptions } = named;
-    const options = {
-      ...allPositionalOptions.reduce((acc, curr) => {
-        return { ...acc, ...curr };
-      }, {}),
-      ...namedOptions,
-    };
-
-    // Ensure that the `modifiers` is always an array
-    const modifiers = isEmpty(options.modifiers)
-      ? []
-      : isArray(options.modifiers)
-      ? options.modifiers
-      : [options.modifiers];
-
-    // Add runloop integration and positional modifiers to the array of modifiers
-    options.modifiers = [
-      ...modifiers,
-      ...allPositionalModifiers,
-      beginRunLoop,
-      endRunLoop,
-    ];
-
-    return options;
-  }
-
   modify(element, positionalArgs, namedArgs) {
     this.primaryElement = element;
     this.secondaryElement = positionalArgs[0];
 
-    this.popperOptions = this.initPopperOptions(positionalArgs, namedArgs);
+    const popperOptions = getPopperOptions(positionalArgs, namedArgs);
 
     // Create the popper once all required arguments are present
     if (!this.popper && this.referenceElement && this.tooltipElement) {
       this.popper = createPopper(
         this.referenceElement,
         this.tooltipElement,
-        this.popperOptions
+        popperOptions
       );
 
       setPopperForElement(this.primaryElement, this.popper);
     }
-    this.popper?.setOptions(this.popperOptions);
+    this.popper?.setOptions(popperOptions);
   }
 
   constructor(owner, args) {
